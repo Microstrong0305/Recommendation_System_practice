f11931a4d3256ff0d28a6af1fcf0ba013766ef98
zhouyanqiang <zhouyanqiang@onething.net>
2020-03-06 19:29:53 +0800

    [add] FMFM-sklearn-TF

--- FM/FM.py
+++ FM/FM.py
@@ -6,79 +6,146 @@ import logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 import numpy as np
 import argparse
-from util import *
+from FM_binaryClass.util import *
 from sklearn.metrics import *
 
 
 class FM(object):
-    def __init__(self, num_classes, k, lr, batch_size, feature_length, reg_l1, reg_l2):
-        self.num_classes = num_classes
-        self.k = k
-        self.lr = lr
-        self.batch_size = batch_size
-        self.p = feature_length
-        self.reg_l1 = reg_l1
-        self.reg_l2 = reg_l2
+    """初始化成员变量"""
 
-    def add_input(self):
-        self.X = tf.placeholder('float32', [None, self.p])
-        self.y = tf.placeholder('float32', [None, self.num_classes])
-        self.keep_prob = tf.placeholder('float32')
+    def __init__(self, feature_size, fm_v_size, loss_fuc, train_optimizer, learning_rate, reg_type, reg_param):
+        # 特征向量长度
+        self.feature_size = feature_size
+        # fm_v_size 向量的长度
+        self.fm_v_size = fm_v_size
+        # 损失函数
+        self.loss_fuc = loss_fuc
+        # 优化方法
+        self.train_optimizer = train_optimizer
+        # 学习率
+        self.learning_rate = learning_rate
+        # 正则类型
+        self.reg_type = reg_type
+        # 正则因子
+        self.reg_param = reg_param
+        # aglobal_step
+        self.glabal_step = tf.Variable(0, dtype=tf.int32, trainable=False, name="global_step")
 
-    def inference(self):
-        with tf.variable_scope('linear_layer'):
-            w0 = tf.get_variable('w0', shape=[self.num_classes],
-                                 initializer=tf.zeros_initializer())
-            self.w = tf.get_variable('w', shape=[self.p, num_classes],
-                                     initializer=tf.truncated_normal_initializer(mean=0, stddev=0.01))
-            self.linear_terms = tf.add(tf.matmul(self.X, self.w), w0)
+    def train(self, batch_data):
+        """ 1 定义输入数据 """
+        with tf.name_scope('input_data'):
+            # 标签：[batch_size, 1]
+            labels = batch_data['labels']
+            # 用户特征向量：[batch_size, feature_size]
+            dense_vector = tf.reshape(batch_data['dense_vector'],
+                                      shape=[-1, self.feature_size, 1])  # None * feature_size * 1
+            print("%s: %s" % ("dense_vector", dense_vector))
+            print("%s: %s" % ("labels", labels))
 
-        with tf.variable_scope('interaction_layer'):
-            self.v = tf.get_variable('v', shape=[self.p, self.k],
-                                     initializer=tf.truncated_normal_initializer(mean=0, stddev=0.01))
-            self.interaction_terms = tf.multiply(0.5,
-                                                 tf.reduce_mean(
-                                                     tf.subtract(
-                                                         tf.pow(tf.matmul(self.X, self.v), 2),
-                                                         tf.matmul(self.X, tf.pow(self.v, 2))),
-                                                     1, keep_dims=True))
-        self.y_out = tf.add(self.linear_terms, self.interaction_terms)
-        if self.num_classes == 2:
-            self.y_out_prob = tf.nn.sigmoid(self.y_out)
-        elif self.num_classes > 2:
-            self.y_out_prob = tf.nn.softmax(self.y_out)
+        """ 2 定义网络输出 """
+        with tf.name_scope("FM_Comput_Score"):
+            # FM参数，生成或者获取W V
+            with tf.variable_scope("fm_layer", reuse=tf.AUTO_REUSE):
+                self.FM_W = tf.get_variable(name='fm_w', shape=[self.feature_size, 1],
+                                            initializer=tf.glorot_normal_initializer())
+                self.FM_V = tf.get_variable(name='fm_v', shape=[self.feature_size, self.fm_v_size],
+                                            initializer=tf.glorot_normal_initializer())
+                self.FM_B = tf.Variable(tf.constant(0.0), dtype=tf.float32, name="fm_bias")  # W0
+            print("%s: %s" % ("FM_W", self.FM_W))
+            print("%s: %s" % ("FM_V", self.FM_V))
+            print("%s: %s" % ("FM_B", self.FM_B))
 
-    def add_loss(self):
-        if self.num_classes == 2:
-            cross_entropy = tf.nn.sigmoid_cross_entropy_with_logits(labels=self.y, logits=self.y_out)
-        elif self.num_classes > 2:
-            cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=self.y, logits=self.y_out)
-        mean_loss = tf.reduce_mean(cross_entropy)
-        self.loss = mean_loss
-        tf.summary.scalar('loss', self.loss)
+            # ---------- w * x----------
+            Y_first = tf.reduce_sum(tf.multiply(self.FM_W, dense_vector), 2)  # None * F
+            print("%s: %s" % ("Y_first", Y_first))
 
-    def add_accuracy(self):
-        # accuracy
-        self.correct_prediction = tf.equal(tf.cast(tf.argmax(self.y_out, 1), tf.float32),
-                                           tf.cast(tf.argmax(self.y, 1), tf.float32))
-        self.accuracy = tf.reduce_mean(tf.cast(self.correct_prediction, tf.float32))
-        # add summary to accuracy
-        tf.summary.scalar('accuracy', self.accuracy)
+            # ---------- Vij * Vij* Xij ---------------
+            embeddings = tf.multiply(self.FM_V, dense_vector)  # None * V * X
+            # sum_square part
+            summed_features_emb = tf.reduce_sum(embeddings, 1)  # sum(v*x)
+            summed_features_emb_square = tf.square(summed_features_emb)  # (sum(v*x))^2
 
-    def train(self):
-        self.global_step = tf.Variable(0, trainable=False)
-        optimizer = tf.train.FtrlOptimizer(self.lr, l1_regularization_strength=self.reg_l1,
-                                           l2_regularization_strength=self.reg_l2)
-        extra_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
-        with tf.control_dependencies(extra_update_ops):
-            self.train_op = optimizer.minimize(self.loss, global_step=self.global_step)
+            # square_sum part
+            squared_features_emb = tf.square(embeddings)  # (v*x)^2
+            squared_sum_features_emb = tf.reduce_sum(squared_features_emb, 1)  # sum((v*x)^2)
 
-    def build_graph(self):
-        self.add_input()
-        self.inference()
-        self.add_loss()
-        self.add_accuracy()
-        self.train()
+            # second order
+            Y_second = 0.5 * tf.subtract(summed_features_emb_square,
+                                         squared_sum_features_emb)  # 0.5*((sum(v*x))^2 - sum((v*x)^2))
+            print("%s: %s" % ("Y_second", Y_second))
+
+            # out = W * X + Vij * Vij* Xij
+            FM_out_lay1 = tf.concat([Y_first, Y_second], axis=1)
+            Y_Out = tf.reduce_sum(FM_out_lay1, 1)
+            # out = out + bias
+            y_d = tf.reshape(Y_Out, shape=[-1])
+            Y_bias = self.FM_B * tf.ones_like(y_d, dtype=tf.float32)  # Y_bias
+            Y_Out = tf.add(Y_Out, Y_bias, name='Y_Out')
+            print("%s: %s" % ("Y_bias", Y_bias))
+            print("%s: %s" % ("Y_Out", Y_Out))
+            # ---------- score ----------
+            score = tf.nn.sigmoid(Y_Out, name='score')
+            score = tf.reshape(score, shape=[-1, 1])
+            print("%s: %s" % ("score", score))
+
+        """ 3 定义损失函数和AUC指标 """
+        with tf.name_scope("loss"):
+            # loss：Squared_error，Cross_entropy ,FTLR
+            if self.reg_type == 'l1_reg':
+                regularization = tf.contrib.layers.l1_regularizer(self.reg_param)(self.FM_W)
+            elif self.reg_type == 'l2_reg':
+                regularization = self.reg_param * tf.nn.l2_loss(self.FM_W)
+            else:
+                regularization = self.reg_param * tf.nn.l2_loss(self.FM_W)
+
+            if self.loss_fuc == 'Squared_error':
+                loss = tf.reduce_mean(tf.reduce_sum(tf.square(labels - score), reduction_indices=[1])) + regularization
+            elif self.loss_fuc == 'Cross_entropy':
+                loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=tf.reshape(Y_Out, [-1]),
+                                                                              labels=tf.reshape(labels,
+                                                                                                [-1]))) + regularization
+            elif self.loss_fuc == 'FTLR':
+                loss = tf.reduce_mean(tf.reduce_sum(tf.square(labels - score), reduction_indices=[1])) + regularization
+            # AUC
+            auc = tf.metrics.auc(labels, score)
+            print("%s: %s" % ("labels", labels))
+            # w为0的比例,w的平均值
+            w_zero_ratio = tf.reduce_mean(tf.to_float(tf.abs(self.FM_W) <= 1.0e-5))
+            w_avg = tf.reduce_mean(self.FM_W)
+            v_zero_ratio = tf.reduce_mean(tf.to_float(tf.abs(self.FM_V) <= 1.0e-5))
+            v_avg = tf.reduce_mean(self.FM_V)
+
+        """ 4 设定optimizer """
+        with tf.name_scope("optimizer"):
+            # ------bulid optimizer------
+            with tf.variable_scope("Optimizer", reuse=tf.AUTO_REUSE):
+                if self.train_optimizer == 'Adam':
+                    optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate, beta1=0.9, beta2=0.999,
+                                                       epsilon=1e-8)
+                elif self.train_optimizer == 'Adagrad':
+                    optimizer = tf.train.AdagradOptimizer(learning_rate=self.learning_rate,
+                                                          initial_accumulator_value=1e-8)
+                elif self.train_optimizer == 'Momentum':
+                    optimizer = tf.train.MomentumOptimizer(learning_rate=self.learning_rate, momentum=0.95)
+                elif self.train_optimizer == 'ftrl':
+                    optimizer = tf.train.FtrlOptimizer(self.learning_rate)
+                train_step = optimizer.minimize(loss, global_step=self.global_step)
+
+        """5 设定summary，以便在Tensorboard里进行可视化 """
+        with tf.name_scope("summaries"):
+            tf.summary.scalar("loss", loss)
+            tf.summary.scalar("accumulate_auc", auc[0])
+            tf.summary.scalar("w_avg", w_avg)
+            tf.summary.scalar("w_zero_ratio", w_zero_ratio)
+            tf.summary.scalar("v_avg", v_avg)
+            tf.summary.scalar("v_zero_ratio", v_zero_ratio)
+            tf.summary.histogram("FM_W", self.FM_W)
+            tf.summary.histogram("FM_V", self.FM_V)
+            # 好几个summary，所以这里要merge_all
+            summary_op = tf.summary.merge_all()
+
+        """6 返回结果 """
+        return Y_Out, score, regularization, loss, auc, train_step, w_zero_ratio, w_avg, v_zero_ratio, v_avg, labels, score, summary_op
 
 
 def train_model(sess, model, epochs=1000, print_every=50):
@@ -191,31 +258,48 @@ if __name__ == '__main__':
     parser.add_argument('--mode', help='train or test', type=str, default='train')
     args = parser.parse_args()
     mode = args.mode
+
     # length of representation
     x_train, y_train, x_test, y_test = load_dataset()
+
     # initialize the model
-    num_classes = 2
-    lr = 0.01
+    feature_size = 530
+    fm_v_size = 20
+    loss_fuc = 'Cross_entropy'
+    train_optimizer = 'Adam'
+    learning_rate = 0.01
+    reg_type = 'l2_reg'
+    reg_param = 0.000
+    log_path = '/data/log/FM_Cross_entropy_L2_0_20180816_01'
     batch_size = 128
-    k = 40
-    reg_l1 = 2e-2
-    reg_l2 = 0
-    feature_length = x_train.shape[1]
+
     # initialize FM model
     batch_gen = batch_generator([x_train, y_train], batch_size)
     test_batch_gen = batch_generator([x_test, y_test], batch_size)
-    model = FM(num_classes, k, lr, batch_size, feature_length, reg_l1, reg_l2)
-    # build graph for model
-    model.build_graph()
+    model = FM(feature_size, fm_v_size, loss_fuc, train_optimizer, learning_rate, reg_type, reg_param)
 
-    saver = tf.train.Saver(max_to_keep=5)
+    Y_Out, score, regularization, loss, auc, train_step, w_zero_ratio, w_avg, v_zero_ratio, v_avg, labels, score, summary_op = model.train(
+        batch_gen)
 
+    init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer(), tf.tables_initializer())
     with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-        check_restore_parameters(sess, saver)
-        if mode == 'train':
-            print('start training...')
-            train_model(sess, model, epochs=1000, print_every=500)
-        if mode == 'test':
-            print('start testing...')
-            test_model(sess, model)
+        sess.run(init_op)
+        batch_cnt = 0
+        # 选定可视化存储目录
+        writer = tf.summary.FileWriter(log_path, sess.graph)
+        try:
+            while True:
+                batch_cnt = batch_cnt + 1
+                a, b, c, d, e, summary = sess.run([loss, auc, w_zero_ratio, w_avg, train_step, summary_op])
+                if batch_cnt % 50 == 0 or batch_cnt <= 10:
+                    y, p = sess.run([labels, score])
+                    if y.sum() > 0.0:
+                        batch_auc = metrics.roc_auc_score(y, p)
+                    else:
+                        batch_auc = 0.0
+                    print(
+                        "batch: {} loss: {:.4f} accumulate_auc: {:.4f} batch_auc: {:.4f} w_zero_ratio: {:.4f} w_avg: {:.4f}".format(
+                            batch_cnt, a, b[0], batch_auc, c, d))
+                    writer.add_summary(summary, batch_cnt)
+        except tf.errors.OutOfRangeError:
+            print("3、Train end of dataset")
