db43186c9361cebea752ebc3711e2645e3523b9f
zhouyanqiang <zhouyanqiang@onething.net>
2020-03-09 18:19:48 +0800

    [add] FM_huangmeiling

--- FM/rs_huangmeiling/FM.py
+++ FM/rs_huangmeiling/FM.py
@@ -1,4 +1,3 @@
-
 # coding: utf-8
 
 # ## 0）环境准备
@@ -23,23 +22,78 @@ import time
 import datetime
 
 import os
-os.environ["CUDA_VISIBLE_DEVICES"]="0"
 
-print tf.__version__
-print tf.__path__
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
+print(tf.__version__)
+print(tf.__path__)
 
 # ## 1）数据准备Dataset格式
 
 # In[2]:
 
-# 每一行解析，解析标签csv格式
-# 0.0,0.6666666666666666,0.5,0.0,0.0,0.0,0.0,0.7272727272727273,0.42857142857142855
+"""
+解析CSV格式，对输入的每一行样本，进行格式解析，返回labels和dense_vector格式数据
+例如输入csv格式字符串： 0.0,0.6666666666666666,0.5,0.0,0.0,0.0,0.0,0.7272727272727273,0.42857142857142855
+函数参数：
+line：需要解析的字符串；
+feature_size：特征长度；
+函数返回：
+返回字典，格式：{'labels': labels, 'dense_vector': dense_vector}
+labels：样本的labels；
+dense_vector：样本的特征向量；
+"""
+
+
+def decode_csv(line, feature_size):
+    # 按照,分割，取label和feature
+    columns = tf.string_split([line], ',')
+    labels = tf.reshape(tf.string_to_number(columns.values[0], out_type=tf.float32), [-1])
+    dense_vector = tf.reshape(tf.string_to_number(columns.values[1:feature_size + 1], out_type=tf.float32),
+                              [feature_size])
+    return {'labels': labels, 'dense_vector': dense_vector}
+
+
+"""
+采用DataSet格式读取文件
+函数参数：
+data_type：文件格式；
+filenames：文件路径；
+batch_size：Batch大小；
+feature_size：特征长度；
+num_epochs：样本复制多少次；
+函数返回：
+返回DataSet
+"""
+
+
+def read_my_file_format(data_type, filenames, feature_size, batch_size, num_epochs=1):
+    # 读取文件
+    print(filenames)
+    dataset = tf.data.TextLineDataset(filenames).map(lambda x: decode_csv(x, feature_size)).prefetch(batch_size).cache()
+    dataset = dataset.repeat(num_epochs)
+    dataset = dataset.batch(batch_size)  # Batch size to use
+    iterator = dataset.make_one_shot_iterator()
+    next_element = iterator.get_next()
+    return next_element
+
+
+# 文件列表
+def get_file_list(my_path):
+    files = []
+    if path.isdir(my_path):
+        [files.append(path.join(my_path, p)) for p in listdir(my_path) if path.isfile(path.join(my_path, p))]
+    else:
+        files.append(my_path)
+    return files
+
+
 # 数据处理
 def process_data(data_type, my_path, feature_size, batch_size=32, num_epochs=1):
     filenames = get_file_list(my_path)
     next_element = read_my_file_format(data_type, filenames, feature_size, batch_size, num_epochs)
     return next_element
+
 
 # 创建session，指定GPU或者CPU使用率
 def get_session(gpu_fraction=0.1):
@@ -55,6 +109,7 @@ def get_session(gpu_fraction=0.1):
 
 class FM(object):
     """ 初始化成员变量 """
+
     def __init__(self, feature_size, fm_v_size, loss_fuc, train_optimizer, learning_rate, reg_type, reg_param):
         # 特征向量长度
         self.feature_size = feature_size
@@ -72,60 +127,64 @@ class FM(object):
         self.reg_param = reg_param
         # aglobal_step
         self.global_step = tf.Variable(0, dtype=tf.int32, trainable=False, name='global_step')
-        
+
     def train(self, batch_data):
         """ 1 定义输入数据 """
         with tf.name_scope('input_data'):
             # 标签：[batch_size, 1]
             labels = batch_data['labels']
             # 用户特征向量：[batch_size, feature_size]
-            dense_vector = tf.reshape(batch_data['dense_vector'], shape=[-1, feature_size, 1]) # None * feature_size * 1
+            dense_vector = tf.reshape(batch_data['dense_vector'],
+                                      shape=[-1, feature_size, 1])  # None * feature_size * 1
             print("%s: %s" % ("dense_vector", dense_vector))
             print("%s: %s" % ("labels", labels))
-            
+
         """ 2 定义网络输出 """
         with tf.name_scope("FM_Comput_Score"):
             # FM参数，生成或者获取W V
             with tf.variable_scope("fm_layer", reuse=tf.AUTO_REUSE):
-                self.FM_W = tf.get_variable(name='fm_w', shape=[self.feature_size, 1], initializer=tf.glorot_normal_initializer())
-                self.FM_V = tf.get_variable(name='fm_v', shape=[self.feature_size, self.fm_v_size], initializer=tf.glorot_normal_initializer())
-                self.FM_B = tf.Variable(tf.constant(0.0), dtype=tf.float32 ,name="fm_bias")  # W0
+                self.FM_W = tf.get_variable(name='fm_w', shape=[self.feature_size, 1],
+                                            initializer=tf.glorot_normal_initializer())
+                self.FM_V = tf.get_variable(name='fm_v', shape=[self.feature_size, self.fm_v_size],
+                                            initializer=tf.glorot_normal_initializer())
+                self.FM_B = tf.Variable(tf.constant(0.0), dtype=tf.float32, name="fm_bias")  # W0
             print("%s: %s" % ("FM_W", self.FM_W))
             print("%s: %s" % ("FM_V", self.FM_V))
             print("%s: %s" % ("FM_B", self.FM_B))
-            
-            # ---------- w * x----------   
+
+            # ---------- w * x----------
             Y_first = tf.reduce_sum(tf.multiply(self.FM_W, dense_vector), 2)  # None * F
             print("%s: %s" % ("Y_first", Y_first))
-            
+
             # ---------- Vij * Vij* Xij ---------------
-            embeddings = tf.multiply(self.FM_V, dense_vector) # None * V * X 
+            embeddings = tf.multiply(self.FM_V, dense_vector)  # None * V * X
             # sum_square part
             summed_features_emb = tf.reduce_sum(embeddings, 1)  # sum(v*x)
             summed_features_emb_square = tf.square(summed_features_emb)  # (sum(v*x))^2
 
             # square_sum part
-            squared_features_emb = tf.square(embeddings) # (v*x)^2
+            squared_features_emb = tf.square(embeddings)  # (v*x)^2
             squared_sum_features_emb = tf.reduce_sum(squared_features_emb, 1)  # sum((v*x)^2)
-            
+
             # second order
-            Y_second = 0.5 * tf.subtract(summed_features_emb_square, squared_sum_features_emb)  # 0.5*((sum(v*x))^2 - sum((v*x)^2))            
+            Y_second = 0.5 * tf.subtract(summed_features_emb_square,
+                                         squared_sum_features_emb)  # 0.5*((sum(v*x))^2 - sum((v*x)^2))
             print("%s: %s" % ("Y_second", Y_second))
-            
+
             # out = W * X + Vij * Vij* Xij
-            FM_out_lay1 = tf.concat([Y_first, Y_second], axis=1) 
+            FM_out_lay1 = tf.concat([Y_first, Y_second], axis=1)
             Y_Out = tf.reduce_sum(FM_out_lay1, 1)
             # out = out + bias
-            y_d = tf.reshape(Y_Out,shape=[-1])
-            Y_bias = self.FM_B * tf.ones_like(y_d, dtype=tf.float32) # Y_bias
-            Y_Out = tf.add(Y_Out, Y_bias, name='Y_Out') 
+            y_d = tf.reshape(Y_Out, shape=[-1])
+            Y_bias = self.FM_B * tf.ones_like(y_d, dtype=tf.float32)  # Y_bias
+            Y_Out = tf.add(Y_Out, Y_bias, name='Y_Out')
             print("%s: %s" % ("Y_bias", Y_bias))
             print("%s: %s" % ("Y_Out", Y_Out))
             # ---------- score ----------  
-            score=tf.nn.sigmoid(Y_Out,name='score')
-            score=tf.reshape(score, shape=[-1, 1])
+            score_1 = tf.nn.sigmoid(Y_Out, name='score')
+            score = tf.reshape(score_1, shape=[-1, 1])
             print("%s: %s" % ("score", score))
-        
+
         """ 3 定义损失函数和AUC指标 """
         with tf.name_scope("loss"):
             # loss：Squared_error，Cross_entropy ,FTLR
@@ -133,13 +192,15 @@ class FM(object):
                 regularization = tf.contrib.layers.l1_regularizer(self.reg_param)(self.FM_W)
             elif reg_type == 'l2_reg':
                 regularization = self.reg_param * tf.nn.l2_loss(self.FM_W)
-            else:  
-                regularization = self.reg_param * tf.nn.l2_loss(self.FM_W)    
-            
+            else:
+                regularization = self.reg_param * tf.nn.l2_loss(self.FM_W)
+
             if loss_fuc == 'Squared_error':
                 loss = tf.reduce_mean(tf.reduce_sum(tf.square(labels - score), reduction_indices=[1])) + regularization
             elif loss_fuc == 'Cross_entropy':
-                loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=tf.reshape(Y_Out, [-1]), labels=tf.reshape(labels, [-1]))) + regularization
+                loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=tf.reshape(Y_Out, [-1]),
+                                                                              labels=tf.reshape(labels,
+                                                                                                [-1]))) + regularization
             elif loss_fuc == 'FTLR':
                 loss = tf.reduce_mean(tf.reduce_sum(tf.square(labels - score), reduction_indices=[1])) + regularization
             # AUC                  
@@ -149,21 +210,22 @@ class FM(object):
             w_zero_ratio = tf.reduce_mean(tf.to_float(tf.abs(self.FM_W) <= 1.0e-5))
             w_avg = tf.reduce_mean(self.FM_W)
             v_zero_ratio = tf.reduce_mean(tf.to_float(tf.abs(self.FM_V) <= 1.0e-5))
-            v_avg = tf.reduce_mean(self.FM_V)            
-            
+            v_avg = tf.reduce_mean(self.FM_V)
+
         """ 4 设定optimizer """
         with tf.name_scope("optimizer"):
-            #------bulid optimizer------
+            # ------bulid optimizer------
             with tf.variable_scope("Optimizer", reuse=tf.AUTO_REUSE):
                 if train_optimizer == 'Adam':
-                    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=0.9, beta2=0.999, epsilon=1e-8)
+                    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=0.9, beta2=0.999,
+                                                       epsilon=1e-8)
                 elif train_optimizer == 'Adagrad':
                     optimizer = tf.train.AdagradOptimizer(learning_rate=learning_rate, initial_accumulator_value=1e-8)
                 elif train_optimizer == 'Momentum':
                     optimizer = tf.train.MomentumOptimizer(learning_rate=learning_rate, momentum=0.95)
                 elif train_optimizer == 'ftrl':
                     optimizer = tf.train.FtrlOptimizer(learning_rate)
-                train_step = optimizer.minimize(loss, global_step=self.global_step)               
+                train_step = optimizer.minimize(loss, global_step=self.global_step)
 
         """5 设定summary，以便在Tensorboard里进行可视化 """
         with tf.name_scope("summaries"):
@@ -177,9 +239,9 @@ class FM(object):
             tf.summary.histogram("FM_V", self.FM_V)
             # 好几个summary，所以这里要merge_all
             summary_op = tf.summary.merge_all()
-            
+
         """6 返回结果 """
-        return Y_Out, score, regularization, loss, auc, train_step, w_zero_ratio, w_avg, v_zero_ratio, v_avg, labels, score, summary_op
+        return Y_Out, score, regularization, loss, auc, train_step, w_zero_ratio, w_avg, v_zero_ratio, v_avg, labels, score_1, summary_op
 
 
 # ## 3）模型训练测试
@@ -187,13 +249,13 @@ class FM(object):
 # In[4]:
 
 # 测试数据
-filenames = '/data/csv-all'
-data_type='csv'
+filenames = r'E:\Recommendation_System_practice\FM\rs_huangmeiling\data'
+data_type = 'csv'
 feature_size = 530
-batch_size = 6000
+batch_size = 64
 num_epochs = 200
-next_element = process_data(data_type, filenames, feature_size, batch_size, num_epochs)  
-    
+next_element = process_data(data_type, filenames, feature_size, batch_size, num_epochs)
+
 # 模型参数   
 feature_size = 530
 fm_v_size = 20
@@ -202,32 +264,35 @@ train_optimizer = 'Adam'
 learning_rate = 0.01
 reg_type = 'l2_reg'
 reg_param = 0.000
-log_path='/data/log/FM_Cross_entropy_L2_0_20180816_01'
+log_path = r"E:\Recommendation_System_practice\FM\rs_huangmeiling\FM_Cross_entropy_L2_0_20180816_01"
 
 # 开始训练
 bea_model = FM(feature_size, fm_v_size, loss_fuc, train_optimizer, learning_rate, reg_type, reg_param)
-Y_Out, score, regularization, loss, auc, train_step, w_zero_ratio, w_avg, v_zero_ratio, v_avg, labels, score, summary_op = bea_model.train(next_element)
+Y_Out, score, regularization, loss, auc, train_step, w_zero_ratio, w_avg, v_zero_ratio, v_avg, labels, score_1, summary_op = bea_model.train(
+    next_element)
 
 init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer(), tf.tables_initializer())
 gpu_fraction = 0.6
-my_device='/gpu:0'
+my_device = '/gpu:0'
 with tf.device(my_device):
     sess = get_session(gpu_fraction)
     sess.run(init_op)
     batch_cnt = 0
-    #选定可视化存储目录
+    # 选定可视化存储目录
     writer = tf.summary.FileWriter(log_path, sess.graph)
     try:
-         while True:
-            batch_cnt = batch_cnt + 1           
+        while True:
+            batch_cnt = batch_cnt + 1
             a, b, c, d, e, summary = sess.run([loss, auc, w_zero_ratio, w_avg, train_step, summary_op])
             if batch_cnt % 50 == 0 or batch_cnt <= 10:
                 y, p = sess.run([labels, score])
                 if y.sum() > 0.0:
-                    batch_auc=metrics.roc_auc_score(y, p)
+                    batch_auc = metrics.roc_auc_score(y, p)
                 else:
-                    batch_auc=0.0
-                print("batch: {} loss: {:.4f} accumulate_auc: {:.4f} batch_auc: {:.4f} w_zero_ratio: {:.4f} w_avg: {:.4f}".format(batch_cnt, a, b[0], batch_auc, c, d))
+                    batch_auc = 0.0
+                print(
+                    "batch: {} loss: {:.4f} accumulate_auc: {:.4f} batch_auc: {:.4f} w_zero_ratio: {:.4f} w_avg: {:.4f}".format(
+                        batch_cnt, a, b[0], batch_auc, c, d))
                 writer.add_summary(summary, batch_cnt)
     except tf.errors.OutOfRangeError:
-        print("3、Train end of dataset")   
+        print("3、Train end of dataset")
